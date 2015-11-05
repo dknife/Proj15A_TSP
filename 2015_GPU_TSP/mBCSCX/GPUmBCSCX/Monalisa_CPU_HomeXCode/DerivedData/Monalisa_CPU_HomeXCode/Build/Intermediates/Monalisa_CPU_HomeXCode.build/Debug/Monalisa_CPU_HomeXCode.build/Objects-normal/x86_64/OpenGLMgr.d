6edac35a7adc61a3ac5f3ec856d2206fa9a5cc23
dknife <ymkang@San302-YMKang.local>
2015-11-05 17:45:30 +0900

    home project delete

--- 2015_GPU_TSP/mBCSCX/GPUmBCSCX/Monalisa_CPU_HomeXCode/DerivedData/Monalisa_CPU_HomeXCode/Build/Intermediates/Monalisa_CPU_HomeXCode.build/Debug/Monalisa_CPU_HomeXCode.build/Objects-normal/x86_64/OpenGLMgr.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/ymkang/myWork_iMac/Mac_5_Development/Proj15A_TSP/2015_GPU_TSP/mBCSCX/GPUmBCSCX/GPUmBCSCX/OpenGLMgr.cpp \
-  /Users/ymkang/myWork_iMac/Mac_5_Development/Proj15A_TSP/2015_GPU_TSP/mBCSCX/GPUmBCSCX/GPUmBCSCX/OpenGLMgr.h
